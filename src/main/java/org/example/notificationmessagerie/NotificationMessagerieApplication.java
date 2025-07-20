1b35c6bb11ca64e9cb5cf4e9adfd4c9f12b25e0e
elouafim <mohammedelouafi02@icloud.com>
2025-07-20 11:16:26 +0100

    mmm

---
+++ src/main/java/org/example/notificationmessagerie/NotificationMessagerieApplication.java
@@ -0,0 +1,13 @@
+package org.example.notificationmessagerie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationMessagerieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationMessagerieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/notificationmessagerie/NotificationMessagerieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.notificationmessagerie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationMessagerieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
